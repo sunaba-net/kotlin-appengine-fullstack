6f5fac4f52d964a8c5fa46254e0486b5dc499d4d
ryohei <katsume@gmail.com>
2020-03-05 11:43:45 +0900

    add appengine utility

---
+++ backend/src/main/kotlin/net/sunaba/appengine/AppEngine.kt
@@ -0,0 +1,63 @@
+package net.sunaba.appengine
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.headers
+import kotlinx.coroutines.*
+import kotlinx.serialization.Serializable
+import kotlin.coroutines.CoroutineContext
+
+
+object AppEngine : CoroutineScope {
+
+    private val job = SupervisorJob()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO + job
+
+    /**
+     *  インスタンスIDが取得できたらサービス環境と判断する
+     */
+    val isServiceEnv: Boolean by lazy { GaeEnv.GAE_INSTANCE.value.isNotEmpty()}
+
+    val isLocalEnv:Boolean = !isServiceEnv
+
+    enum class GaeEnv(val key:String) {
+        GAE_APPLICATION("GAE_APPLICATION"),
+        GAE_DEPLOYMENT_ID("GAE_DEPLOYMENT_ID"),
+        GOOGLE_CLOUD_PROJECT("GOOGLE_CLOUD_PROJECT"),
+        GAE_SERVICE("GAE_SERVICE"),
+        GAE_VERSION("GAE_VERSION"),
+        GAE_INSTANCE("GAE_INSTANCE");
+        val value:String by lazy {
+            System.getenv(key)?:""
+        }
+    }
+
+    enum class MetaPath(val path: String) {
+        NUMERIC_PROJECT_ID("/computeMetadata/v1/project/numeric-project-id"),
+        PROJECT_ID("/computeMetadata/v1/project/project-id"),
+        ZONE("/computeMetadata/v1/instance/zone"),
+        SERVICE_ACCOUNT_DEFAULT_ALIASES("/computeMetadata/v1/instance/service-accounts/default/aliases"),
+        SERVICE_ACCOUNT_DEFAULT("/computeMetadata/v1/instance/service-accounts/default/"),
+        SERVICE_ACCOUNT_DEFAULT_SCOPES("/computeMetadata/v1/instance/service-accounts/default/scopes")
+    }
+
+    /**
+     * @see https://cloud.google.com/compute/docs/storing-retrieving-metadata?hl=ja
+     */
+    val metaData: Map<MetaPath, String> by lazy {
+        runBlocking {
+            val client = HttpClient()
+            MetaPath.values().map {
+                it to async {
+                    client.get<String>("http://metadata.google.internal${it.path}") {
+                        headers {
+                            append("Metadata-Flavor", "Google")
+                        }
+                    }
+                }
+            }.map { it.first to it.second.await() }.toMap()
+        }
+    }
+}
