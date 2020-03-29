1925ce6a76515434aa2326a0d60bcde6cff20190
ryohei <katsume@gmail.com>
2020-03-30 06:49:31 +0900

    more work.

--- react-hands-on/src/main/kotlin/main.kt
+++ react-hands-on/src/main/kotlin/main.kt
@@ -1,29 +1,21 @@
+import kotlinx.coroutines.*
 import kotlinx.css.*
 import kotlinx.html.js.onClickFunction
-import kotlinx.html.onClick
 import react.*
 import react.dom.*
 import styled.css
+import styled.styledButton
 import styled.styledDiv
 import kotlin.browser.document
 import kotlin.browser.window
 
-val unwatchedVideos = listOf(
-        Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
-        Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
-        Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
-)
-
-val watchedVideos = listOf(
-        Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
-)
-
 interface VideoListProps : RProps {
     var videos: List<Video>
+    var selectedVideo:Video?
+    var onSelectVideo:(Video)->Unit
 }
 
 interface VideoListState:RState {
-    var selectedVideo:Video?
 }
 
 class VideoList : RComponent<VideoListProps, VideoListState>() {
@@ -33,10 +25,10 @@ class VideoList : RComponent<VideoListProps, VideoListState>() {
                 key = video.id.toString()
                 attrs {
                     onClickFunction = {
-                        setState { selectedVideo = video }
+                        props.onSelectVideo(video)
                     }
                 }
-                if (video == state.selectedVideo) {
+                if (video == props.selectedVideo) {
                     + "▶ "
                 }
                 +"${video.speaker}: ${video.title}"
@@ -45,17 +37,116 @@ class VideoList : RComponent<VideoListProps, VideoListState>() {
     }
 }
 
-fun RBuilder.videoList(handler: VideoListProps.() -> Unit): ReactElement = child(VideoList::class) {
-    attrs(handler)
-}
+fun RBuilder.videoList(handler: VideoListProps.() -> Unit) = child(VideoList::class) { attrs(handler) }
 
 data class Video(val id: Int, val title: String, val speaker: String, val videoUrl: String)
 
-interface AppState: RState {
-    var currentVideo: Video?
+interface VideoPlayerProps : RProps {
+    var video:Video
+    var onWatchedButtonPressed:(Video)->Unit
+    var unwatchedVideo:Boolean
 }
 
+class VideoPlayer: RComponent<VideoPlayerProps, RState>() {
+    override fun RBuilder.render() {
+        styledDiv {
+            css {
+                position = Position.absolute
+                top = 10.px
+                right = 10.px
+            }
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+
+            styledButton {
+                css {
+                    display = Display.block
+                    backgroundColor = if (props.unwatchedVideo) Color.lightGreen else Color.red
+                }
+                attrs {
+                    onClickFunction = {
+                        props.onWatchedButtonPressed(props.video)
+                    }
+                }
+                if (props.unwatchedVideo) {
+                    + "Mark as watched"
+                }
+                else {
+                    + "Mark as unwatched"
+                }
+            }
+
+            styledDiv {
+                css {
+                    display = Display.flex
+                    marginBottom = 10.px
+                }
+                EmailShareButton {
+                    attrs.url = props.video.videoUrl
+                    EmailIcon {
+                        attrs.size = 32
+                        attrs.round = true
+                    }
+                }
+
+                TelegramShareButton {
+                    attrs.url = props.video.videoUrl
+                    TelegramIcon {
+                        attrs.size = 32
+                        attrs.round = true
+                    }
+                }
+            }
+
+            ReactPlayer {
+                attrs.url = props.video.videoUrl
+            }
+        }
+    }
+}
+
+fun RBuilder.videoPlayer(handler:VideoPlayerProps.()->Unit) = child(VideoPlayer::class) {attrs(handler)}
+
+interface AppState: RState {
+    var currentVideo: Video?
+    var unwatchedVideos:List<Video>
+    var watchedVideos:List<Video>
+}
+
+
+
 class App : RComponent<RProps, AppState>() {
+
+    suspend fun fetchVideo(id: Int): Video =
+            window.fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
+                    .await()
+                    .json()
+                    .await()
+                    .unsafeCast<Video>()
+
+    suspend fun fetchVideos() = coroutineScope {
+        (1..25).map { id ->
+            async {
+                fetchVideo(id)
+            }
+        }.awaitAll()
+    }
+
+    override fun AppState.init() {
+        unwatchedVideos = listOf()
+
+        watchedVideos = listOf()
+        val mainScope = MainScope()
+        mainScope.launch {
+            val videos = fetchVideos()
+            setState {
+                unwatchedVideos = videos
+            }
+        }
+
+    }
+
     override fun RBuilder.render() {
         h1 {
             +"KotlinConf Explorer"
@@ -64,32 +155,51 @@ class App : RComponent<RProps, AppState>() {
             h3 {
                 +"Videos to watch"
             }
-            videoList { videos = unwatchedVideos }
+            videoList {
+                videos = state.unwatchedVideos
+                selectedVideo = state.currentVideo
+                onSelectVideo = {video->
+                    setState {
+                        currentVideo = video
+                    }
+                }
+            }
 
 
             h3 {
                 +"Videos watched"
             }
-            videoList { videos = watchedVideos }
-
-        }
-        div {
-            styledDiv {
-                css {
-                    position = Position.absolute
-                    top = 10.px
-                    right = 10.px
+            videoList {
+                videos = state.watchedVideos
+                selectedVideo = state.currentVideo
+                onSelectVideo = {video->
+                    setState {
+                        currentVideo = video
+                    }
                 }
-                h3 {
-                    +"John Doe: Building and breaking things"
-                }
-                img {
-                    attrs {
-                        src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+            }
+            state.currentVideo?.let {currentVideo->
+                videoPlayer {
+                    video = currentVideo
+                    unwatchedVideo = currentVideo in state.unwatchedVideos
+                    onWatchedButtonPressed = {
+                        if (video in state.unwatchedVideos) {
+                            setState {
+                                unwatchedVideos -= video
+                                watchedVideos += video
+                            }
+                        }
+                        else {
+                            setState {
+                                unwatchedVideos += video
+                                watchedVideos -= video
+                            }
+                        }
                     }
                 }
             }
         }
+
     }
 }
 
